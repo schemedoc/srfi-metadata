047e494a7cd29ae01df72970b55d34bd987e05ac
Lassi Kortela <lassi@lassi.io>
2020-05-02 18:15:11 +0300

    Autogen Larceny

---
+++ listings/larceny.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu -o pipefail
+cd "$(dirname "$0")"
+curl --fail --silent --show-error --location \
+    https://github.com/larcenists/larceny/archive/master.tar.gz |
+    gunzip |
+    tar -tf - |
+    sed 's@%3a@@' |
+    grep -E 'srfi.[0-9]+\.s' |
+    grep -v in-progress |
+    grep -oE 'srfi.[0-9]+' |
+    grep -oE '[0-9]+' |
+    sort -g |
+    uniq >larceny.scm
