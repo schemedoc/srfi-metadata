569467af6022627ff5c07cb2999a797c17e57771
Lassi Kortela <lassi@lassi.io>
2020-05-03 11:25:48 +0300

    Autogen TinyScheme

---
+++ listings/tinyscheme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu -o pipefail
+cd "$(dirname "$0")"
+curl --fail --silent --show-error \
+    https://sourceforge.net/p/tinyscheme/code/HEAD/tree/trunk/init.scm?format=raw |
+    grep -oE 'srfi-[0-9]+ ' |
+    grep -oE '[0-9]+' |
+    sort -g |
+    uniq >tinyscheme.scm
