3dfb330d5069c1f0311d5455824003abcda54355
Lassi Kortela <lassi@lassi.io>
2020-05-03 11:11:32 +0300

    Untabify

--- listings/bigloo.sh
+++ listings/bigloo.sh
@@ -2,8 +2,8 @@
 set -eu -o pipefail
 cd "$(dirname "$0")"
 curl --fail --silent --show-error \
-	https://raw.githubusercontent.com/manuel-serrano/bigloo/master/manuals/srfi.texi |
-	grep -oE '^@item @code{srfi-[0-9]+} ' |
-	grep -oE '[0-9]+' |
-	sort -g |
-	uniq >bigloo.scm
+    https://raw.githubusercontent.com/manuel-serrano/bigloo/master/manuals/srfi.texi |
+    grep -oE '^@item @code{srfi-[0-9]+} ' |
+    grep -oE '[0-9]+' |
+    sort -g |
+    uniq >bigloo.scm
