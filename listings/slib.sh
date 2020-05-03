6ad7c9ffae983623b49e1c5475998ee9719ce546
Lassi Kortela <lassi@lassi.io>
2020-05-03 11:11:04 +0300

    Autogen SLIB

---
+++ listings/slib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu -o pipefail
+cd "$(dirname "$0")"
+curl --fail --silent --show-error \
+    "http://cvs.savannah.gnu.org/viewvc/*checkout*/slib/slib/slib.texi" |
+    grep -E 'srfi-[0-9]+' |
+    grep -oE '[0-9]+' |
+    sort -g |
+    uniq >slib.scm
