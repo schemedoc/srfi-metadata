fbafc423d973f47ccea55d5c33702bb982fcaadb
Lassi Kortela <lassi@lassi.io>
2020-05-03 20:54:58 +0300

    Scrape Ypsilon SRFIs from latest release
    Thanks to @erkin for finding the listing that I missed.
    
    Closes #2

---
+++ listings/ypsilon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu -o pipefail
+cd "$(dirname "$0")"
+curl --fail --silent --show-error \
+    https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/ypsilon/ypsilon-0.9.6.update3.tar.gz |
+    gunzip |
+    tar -tf - |
+    sed 's@%3a@@' |
+    grep sitelib/srfi/ |
+    grep -oE '[0-9]+.scm' |
+    grep -oE '[0-9]+' |
+    sort -g |
+    uniq >ypsilon.scm
