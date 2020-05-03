0947225c75c09865d5cb62be5307109512657875
Lassi Kortela <lassi@lassi.io>
2020-05-03 11:50:22 +0300

    Autogen Chicken

---
+++ listings/chicken.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu -o pipefail
+cd "$(dirname "$0")"
+curl --fail --silent --show-error \
+    http://wiki.call-cc.org/supported-standards |
+    grep -oE '>SRFI-[0-9]+<' |
+    grep -oE '[0-9]+' |
+    sort -g |
+    uniq >chicken.scm
