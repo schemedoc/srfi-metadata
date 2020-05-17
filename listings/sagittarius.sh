2b541a1328a7a9d4f051471cd74f671a800f6696
Erkin B Altunbas <me@erkin.party>
2020-05-17 11:33:01 +0300

    Alter Sagittarius scraping regexp to match SRFIs without long-form names

--- listings/sagittarius.sh
+++ listings/sagittarius.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 cd "$(dirname "$0")"
 curl --fail --silent --show-error \
     https://bitbucket.org/ktakashi/sagittarius-scheme/raw/master/doc/srfi.scrbl |
-    grep -oE '\(srfi :[0-9]+ ' |
+    grep -oE '\(srfi :[0-9]+[ )]' |
     grep -oE '[0-9]+' |
     sort -g |
     uniq >sagittarius.scm
