321733a8c36f1479d959954362dc17a5e29aac3c
erkin <me@erkin.party>
2020-07-09 00:41:49 +0300

    Update table

--- listings/loko-head.sh
+++ listings/loko-head.sh
@@ -2,9 +2,9 @@
 set -eu -o pipefail
 cd "$(dirname "$0")"
 curl --fail --silent --show-error --location \
-    https://gitlab.com/weinholt/loko/-/archive/master.tar.gz |
+    https://github.com/weinholt/loko/archive/master.tar.gz |
     gunzip |
-    tar -xf - --to-stdout --wildcards 'loko-master-*/Documentation/manual/lib-std.texi' |
+    tar -xf - --to-stdout 'loko-master/Documentation/manual/lib-std.texi' |
     grep -oE '^@code{\(srfi :[0-9]+ ' |
     grep -oE '[0-9]+' |
     sort -g |
