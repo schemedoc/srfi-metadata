50e4e380d262f2d42a5b7499e441b48ec091eab8
Lassi Kortela <lassi@lassi.io>
2020-08-21 13:20:16 +0300

    Fix STklos scraper
    The data looks like this:
    
    (define *srfis*
       '((0  . "Feature-based conditional expansion construct")
         (1  . "List Library")
         (2  . "AND-LET*: an AND with local bindings, a guarded LET* ...")
         ...))
    
    The scraper missed the 0 line since it starts with '(( instead of (
    
    Thanks to @jpellegrini for noticing this.

--- listings/stklos.sh
+++ listings/stklos.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 cd "$(dirname "$0")"
 curl --fail --silent --show-error \
     https://raw.githubusercontent.com/egallesio/STklos/master/doc/skb/srfi.stk |
-    grep -oE '^ +\([0-9]+ +\. "' |
+    grep -oE '^ +'\''?\(?\([0-9]+ +\. "' |
     grep -oE '[0-9]+' |
     sort -g |
     uniq >stklos.scm
