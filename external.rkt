4f00527e7bf9f54de6ebdb121e653d3c41a2139f
Lassi Kortela <lassi@lassi.io>
2020-11-16 17:47:46 +0200

    Scrape Chicken 5 from wiki and egg-locations

--- external.rkt
+++ external.rkt
@@ -2,6 +2,36 @@
 (require net/base64 net/http-client net/url)
 (require json)
 (require sxml/sxpath)
+(require (only-in rnrs/io/ports-6 call-with-port))
+(require (only-in srfi/13 string-contains))
+
+(define (read-all)
+  (let loop ((xs '()))
+    (let ((x (read)))
+      (if (eof-object? x) (reverse xs) (loop (cons x xs))))))
+
+(define (write-sexp-file filename forms)
+  (call-with-atomic-output-file
+   filename
+   (lambda (port _)
+     (parameterize ((current-output-port port))
+       (for-each writeln forms)))))
+
+(define (command->string command . args)
+  (let-values (((sub stdout stdin stderr)
+                (apply subprocess #f
+                       (current-input-port)
+                       (current-error-port)
+                       (find-executable-path command)
+                       args)))
+    (let ((output (port->string stdout)))
+      (subprocess-wait sub)
+      (if (eqv? 0 (subprocess-status sub))
+          output
+          (error "Command failed:" (cons command args))))))
+
+(define (call-with-url-port url proc)
+  (call-with-port (get-pure-port (string->url url)) proc))
 
 (define (query-github query)
   (read-json (get-pure-port (string->url (string-append "https://api.github.com" query)))))
@@ -39,12 +69,7 @@
     (thunk (for-each displayln results)
            (displayln 160))))
 
-(displayln "CHICKEN...")
-;; GitHub API returns content in base64 format embedded in a JSON object.
-(let* ((response (query-github "/repos/diamond-lizard/chicken-srfi-support/contents/srfi-table.org"))
-       (table (string-split (base64-decode-string (hash-ref response 'content)) "\n"))
-       (chicken-port (open-output-file "data/chicken.scm" #:exists 'replace))
-       (egg-port (open-output-file "data/chicken-external.scm" #:exists 'replace)))
+(define (parse-org-mode-table table chicken-port egg-port)
   ;; Ad hoc, quick-and-dirty org-mode table parser
   (define (get-srfi-number cells)
     (cadr (regexp-match #rx"\\[\\[.+\\]\\[(.+)\\]\\]" (string-trim (list-ref cells 6)))))
@@ -56,8 +81,43 @@
         (when (not-space? (cadr cells)) ; Second row = egg
           (displayln (get-srfi-number cells) egg-port)))
       ;; Every other line is a horizontal rule, so we always skip one.
-      (loop (cddr lines))))
-  (close-output-port chicken-port)
-  (close-output-port egg-port))
+      (loop (cddr lines)))))
 
+(define (get-chicken-built-in-srfis)
+  (let* ((whole-page (call-with-url-port
+                      "https://wiki.call-cc.org/supported-standards"
+                      port->string))
+         (c3-c4-start (string-contains
+                       whole-page "<h4 id=\"chicken-3-and-4\">"))
+         (c5-part (substring whole-page 0 c3-c4-start)))
+    (map (lambda (srfi-nnn)
+           (string->number (substring srfi-nnn (string-length "srfi-"))))
+         (regexp-match* #rx"SRFI-[0-9]+" c5-part))))
+
+(define (get-chicken-external-srfis)
+  (sort
+   (filter-map
+    (lambda (egg)
+      (let ((name (symbol->string (car egg))))
+        (cond ((string=? name "vector-lib") 43)
+              ((string=? name "box") 111)
+              (else
+               (and (string-prefix? name "srfi-")
+                    (string->number
+                     (substring name (string-length "srfi-"))))))))
+    (with-input-from-string
+        (command->string
+         "curl"
+         "--location"
+         "--fail"
+         "--silent"
+         "--show-error"
+         "--user" "anonymous:"
+         "https://code.call-cc.org/svn/chicken-eggs/release/5/egg-locations")
+      read-all))
+   <))
+
+(displayln "CHICKEN...")
+(write-sexp-file "data/chicken.scm" (get-chicken-built-in-srfis))
+(write-sexp-file "data/chicken-external.scm" (get-chicken-external-srfis))
 (displayln "Scraped.")
