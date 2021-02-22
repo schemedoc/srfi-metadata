8ccd76fd056110c393bbf21e1b79837aaff724aa
Jeronimo Pellegrini <j_p@aleph0.info>
2021-02-22 19:10:12 -0300

    STklos 1.60

--- listings/stklos.sh
+++ listings/stklos.sh
@@ -3,9 +3,9 @@
 set -eu -o pipefail
 cd "$(dirname "$0")"
 curl --fail --silent --show-error --location \
-	https://github.com/egallesio/STklos/archive/stklos-1.50.tar.gz |
+	https://github.com/egallesio/STklos/archive/stklos-1.60.tar.gz |
 	gunzip |
-	${TAR:-tar} -xf - --to-stdout --wildcards 'STklos-stklos-1.50/SUPPORTED-SRFIS' |
+	${TAR:-tar} -xf - --to-stdout --wildcards 'STklos-stklos-1.60/SUPPORTED-SRFIS' |
 	grep -oE 'SRFI-[0-9]+:' |
 	grep -oE '[0-9]+' |
 	sort -g |
