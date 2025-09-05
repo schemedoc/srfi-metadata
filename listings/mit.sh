67c6bd4b4dbf31b86d598f9dc80ce9e5f2d440a7
Jeronimo Pellegrini <j_p@aleph0.info>
2025-09-05 11:45:29 -0300

    Fix URL for MIT Scheme

--- listings/mit.sh
+++ listings/mit.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 cd "$(dirname "$0")"
 curl --fail --silent --show-error \
-    https://git.savannah.gnu.org/cgit/mit-scheme.git/plain/etc/standards/standards.scm |
+    https://cgit.git.savannah.gnu.org/cgit/mit-scheme.git/plain/etc/standards/standards.scm |
     grep -ioE "(srfi [0-9]+)" |
     grep -oE '[0-9]+' |
     sort -g |
