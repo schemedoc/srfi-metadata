b4b041fa3440aaa9d6932bb28a8fb9b0a64e5ef0
Jeronimo Pellegrini <j_p@aleph0.info>
2025-09-25 07:32:10 -0300

    Update and regenerate table
    Except fo Chickenm which I'm not sure the scripts are generating
    correctly.

--- listings/slib.sh
+++ listings/slib.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 cd "$(dirname "$0")"
-curl --fail --silent --show-error \
-    "http://cvs.savannah.gnu.org/viewvc/*checkout*/slib/slib/slib.texi" |
-    grep -E 'srfi-[0-9]+' |
-    grep -oE '[0-9]+' |
-    sort -g |
-    uniq >../data/slib.pose
+#curl --fail --silent --show-error \
+#    "http://cvs.savannah.gnu.org/viewvc/*checkout*/slib/slib/slib.texi" |
+#    grep -E 'srfi-[0-9]+' |
+#    grep -oE '[0-9]+' |
+#    sort -g |
+#    uniq >../data/slib.pose
